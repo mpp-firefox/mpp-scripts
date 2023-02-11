556a77d53f07f3f0cc7ad359e8d63ca20f8e61f9
mpp-firefox <122710663+mpp-firefox@users.noreply.github.com>
2023-02-12 00:52:19 +0100

    Second Color 1.6

--- MPP Second Color.user.js
+++ MPP Second Color.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         MPP Second Color
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  Ability to have a second color in MPP
-// @author       MPP Firefox
+// @author       MPP Firefox, Anonygold
 // @match        https://multiplayerpiano.com/*
 // @match        https://mppclone.com/*
+// @match        https://mpp.141.lv/*
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAACDSURBVDhPY2TABooL/kNZBAGKAXxFOURrhAG4AXKF6SRrBgGwAXqFyWRpBgGwAXYFceQb4JcfRbZmEGCMyw+jzIC8vEDKDKjL86XMgP5cD8oMmJ/jQpkBIGJ9jj3ZhoAN2J9tTZkBIHA+y5wsQ+AGgMD9LCOSDUExAAbeZ+oRaRADAwAarSM3FaaFtwAAAABJRU5ErkJggg==
 // @grant        none
 // ==/UserScript==
@@ -16,8 +17,10 @@
 //
 //
 
+//   thank Anonygold for the "second color at top" option, ok?
+
 const _sc_init = () => {
-    const _sc_version = "1.5";
+    const _sc_version = "1.6";
     let _sc_actualVersion;
     window._sc_users = {};
 
@@ -25,37 +28,64 @@ const _sc_init = () => {
 
     //if (window.localStorage.getItem("secondColor") === null) window.localStorage.setItem("secondColor", "#000000");
 
+    let currentSecondColor = window.localStorage.getItem("secondColor") || '#777777';
+    let currentAtTop = (window.localStorage.getItem("atTop") === 'true') || false;
+
     const _sc_colorInvalid = sc => {
         return typeof sc !== "string" || !CSS.supports("color", sc) || sc.length !== 7;
     }
 
-    const _sc_sendColor = (sc, global, user, req) => {
+    const _sc_sendColor = (sc, atTop, global, user, req) => {
         if (_sc_colorInvalid(sc)) sc = MPP.client.getOwnParticipant().color;
 
         window.localStorage.setItem("secondColor", sc);
 
-        if (global) MPP.client.sendArray([{m: "custom", data: {sc: sc, requestSc: req}, target: {mode: "subscribed"}}]);
-        else MPP.client.sendArray([{m: "custom", data: {sc: sc, requestSc: req}, target: {mode: "id", id: user}}]);
+        currentSecondColor = sc;
+
+        const top = String(atTop) === 'true';
+
+        window.localStorage.setItem("atTop", top);
+
+        currentAtTop = top;
+
+        if (global) MPP.client.sendArray([{m: "custom", data: {sc, atTop: top, requestSc: req}, target: {mode: "subscribed"}}]);
+        else MPP.client.sendArray([{m: "custom", data: {sc, atTop: top, requestSc: req}, target: {mode: "id", id: user}}]);
     }
 
     MPP.client.on('hi', () => MPP.client.sendArray([{m: "+custom"}]));
 
     MPP.client.on('ch', () => {
-        let sc = window.localStorage.getItem("secondColor");
+        let sc = currentSecondColor;
         if (sc === null) window.localStorage.setItem("secondColor", MPP.client.getOwnParticipant().color), sc = window.localStorage.getItem("secondColor");
-        if (_sc_colorInvalid(sc)) sc = MPP.client.getOwnParticipant().color;
+        const participant = MPP.client.getOwnParticipant();
+        if (_sc_colorInvalid(sc)) sc = participant.color;
 
-        MPP.client.getOwnParticipant().nameDiv.style.background = `linear-gradient(${MPP.client.getOwnParticipant().color}, ${sc})`;
+        const background = currentAtTop ? `linear-gradient(${
+            sc
+        }, ${
+            participant.color
+        })` : `linear-gradient(${
+            participant.color
+        }, ${
+            sc
+        })`;
 
-        _sc_sendColor(sc, true, "", true);
+        if (participant.nameDiv) {
+            participant.nameDiv.style.background = background;
+        }
+        if (participant.cursorDiv) {
+            participant.cursorDiv.getElementsByClassName("name")[0].style.background = background;
+        }
+
+        _sc_sendColor(sc, currentAtTop, true, "", true);
     });
 
     MPP.client.on('p', msg => {
-        let sc = window.localStorage.getItem("secondColor");
+        let sc = currentSecondColor;
         if (sc === null) window.localStorage.setItem("secondColor", MPP.client.getOwnParticipant().color), sc = window.localStorage.getItem("secondColor");
         if (_sc_colorInvalid(sc)) sc = MPP.client.getOwnParticipant().color;
 
-        _sc_sendColor(sc, false, msg._id, true);
+        _sc_sendColor(sc, currentAtTop, false, msg._id, true);
     });
 
     MPP.client.on('custom', msg => {
@@ -65,8 +95,22 @@ const _sc_init = () => {
 
             if (_sc_colorInvalid(sc)) sc = participant.color;
 
-            participant.nameDiv.style.background = `linear-gradient(${participant.color}, ${sc})`;
-            participant.cursorDiv.getElementsByClassName("name")[0].style.background = `linear-gradient(${participant.color}, ${sc})`;
+            const background = msg.data.atTop ? `linear-gradient(${
+                sc
+            }, ${
+                participant.color
+            })` : `linear-gradient(${
+                participant.color
+            }, ${
+                sc
+            })`;
+
+            if (participant.nameDiv) {
+              participant.nameDiv.style.background = background;
+            }
+            if (participant.cursorDiv) {
+              participant.cursorDiv.getElementsByClassName("name")[0].style.background = background;
+            }
 
             if (participant.tag === undefined)
                 participant.nameDiv.title = "This user is using Second Color."; else switch (participant.tag.text) {
@@ -96,27 +140,33 @@ const _sc_init = () => {
                     participant.nameDiv.title = "This is a well known person on Twitch, Youtube, or another platform and is using Second Color.";
                     break;
             }
-            
+
             window._sc_users[participant._id] = participant.name;
 
             if (_sc_colorInvalid(window.localStorage.getItem("secondColor"))) window.localStorage.setItem("secondColor", MPP.client.getOwnParticipant().color);
             let _sc = window.localStorage.getItem("secondColor");
 
-            if (msg.data.requestSc) _sc_sendColor(_sc, false, msg.p);
+            if (msg.data.requestSc && msg.p !== MPP.client.getOwnParticipant().id) _sc_sendColor(_sc, currentAtTop, false, msg.p);
         }
     });
 
     $("#rename .submit").click(() => {
-        let participant = MPP.client.getOwnParticipant();
+        const participant = MPP.client.getOwnParticipant();
 
-        let name = $("#rename input[name=name]").val();
-        let fcolor = $("#rename input[name=color]").val();
-        let scolor = $("#rename input[name=secondcolor]").val();
+        const name = $("#rename input[name=name]").val();
+        const fcolor = $("#rename input[name=color]").val();
+        const scolor = $("#rename input[name=secondcolor]").val();
+        const top = document.querySelector('#rename input[id="second-color-at-top"]').checked;
 
-        MPP.client.sendArray([{m: "userset", set: {name: name, color: fcolor}}]);
-        if (scolor !== window.localStorage.getItem("secondColor")) _sc_sendColor(scolor, true);
+        MPP.client.sendArray([{m: "userset", set: {name, color: fcolor}}]);
+        if (scolor !== window.localStorage.getItem("secondColor") || top !== (window.localStorage.getItem("atTop") === 'true')) _sc_sendColor(scolor, top, true);
 
-        participant.nameDiv.style.background = `linear-gradient(${fcolor}, ${scolor})`;
+        if (participant.nameDiv) {
+          participant.nameDiv.style.background = top ? `linear-gradient(${scolor}, ${fcolor})` : `linear-gradient(${fcolor}, ${scolor})`;
+        }
+        if (participant.cursorDiv) {
+          participant.cursorDiv.style.background = top ? `linear-gradient(${scolor}, ${fcolor})` : `linear-gradient(${fcolor}, ${scolor})`;
+        }
     });
 
     const _sc_createButtons = () => {
@@ -135,12 +185,11 @@ const _sc_init = () => {
         button.setAttribute("maxlength", 7);
         button.setAttribute("name", "secondcolor");
 
-        $("#rename p").append(button);
-
-        let _button = document.createElement("button");
+        const _button = document.createElement("button");
 
         _button.innerText = "Invert";
         _button.setAttribute("class", "ugly-button");
+        _button.setAttribute("id", "invert");
         _button.style = "margin-left: 10px; width: 50px; height: 27px; user-select: none; color: white";
 
         _button.onclick = () => {
@@ -149,7 +198,24 @@ const _sc_init = () => {
             button.value = ___yes;
         }
 
-        $("#rename p").append(_button);
+        const label = document.createElement("label");
+
+        label.innerText = ' Second color at top: ';
+        label.style.fontSize = '16px';
+        label.setAttribute("id", "scattop");
+
+        const checkbox = document.createElement("input");
+
+        checkbox.type = 'checkbox';
+        checkbox.id = 'second-color-at-top';
+        checkbox.checked = currentAtTop;
+
+        label.appendChild(checkbox);
+
+        $("#rename p").append(button).append(_button).append(label);
+
+        let _br = document.createElement("br");
+        $("#rename p #scattop")[0].before(_br);
     }
 
     _sc_createButtons();
@@ -164,9 +230,9 @@ const _sc_init = () => {
                 });
             }
         }));
-        
+
     }, 3000);
-    
+
 };
 
 const _sc_mppExists = time => {
